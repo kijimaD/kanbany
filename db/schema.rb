27a2e79fc2360911d82af0ad9166dcf86af0b943
kijima <norimaking777@gmail.com>
2020-12-06 19:37:56 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_06_101559) do
+ActiveRecord::Schema.define(version: 2020_12_06_103056) do
+
+  create_table "boards", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
