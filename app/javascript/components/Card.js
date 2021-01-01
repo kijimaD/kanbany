6e372389350d7e3da8c59c5a5f3c4c2c6fbbbfec
kijima <norimaking777@gmail.com>
2021-01-01 16:07:48 +0900

    Add style component test

--- app/javascript/components/Card.js
+++ app/javascript/components/Card.js
@@ -52,7 +52,7 @@ class Card extends React.Component {
     render () {
         return (
             <div className="Card">
-              <div className="CardHeader">
+		<div className="CardHeader" style={{ borderBottomColor: 'black' }}>
                 <input type="text" value={this.state.form.name} placeholder="Title" onChange={e=>this.handleChange(e, "name", this.props.id)} />
                 <button className="ContentButton btn btn-sm btn-outline-danger float-right" onClick={() => this.props.handleDelete(this.props.id, this.props.column_id)} tabIndex="-1" >
                   <i className="far fa-trash-alt"></i>
