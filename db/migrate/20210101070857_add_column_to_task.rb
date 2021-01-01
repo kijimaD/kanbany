ad34cfe76b498ee812d64268c23729c2868bb262
kijima <norimaking777@gmail.com>
2021-01-01 16:12:10 +0900

    migration

--- db/migrate/20210101070857_add_column_to_task.rb
+++ db/migrate/20210101070857_add_column_to_task.rb
@@ -1,5 +1,5 @@
 class AddColumnToTask < ActiveRecord::Migration[6.0]
   def change
-    add_column :tasks, :color, :string
+    add_column :tasks, :color, :string, null: false, default: "black"
   end
 end
