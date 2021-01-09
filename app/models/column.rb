bda2ee846cb9543c05008fc60ee518e8de79fb70
kijima <norimaking777@gmail.com>
2021-01-09 11:27:00 +0900

    Change include position

--- app/models/column.rb
+++ app/models/column.rb
@@ -1,8 +1,8 @@
 class Column < ApplicationRecord
+  include RankedModel
   belongs_to :board
   has_many :tasks, dependent: :destroy
   has_many :tasks, ->{ order('row_order asc') }
 
-  include RankedModel
   ranks :row_order, :with_same => :board_id
 end
