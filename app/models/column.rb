794ebcf9bb027d010cb89d010bf9cb1555c51825
kijima <norimaking777@gmail.com>
2021-01-11 11:38:43 +0900

    Fix dependent error

--- app/models/column.rb
+++ app/models/column.rb
@@ -1,8 +1,7 @@
 class Column < ApplicationRecord
   include RankedModel
   belongs_to :board
-  has_many :tasks, dependent: :destroy
-  has_many :tasks, ->{ order('row_order asc') }
+  has_many :tasks, ->{ order('row_order asc') }, dependent: :destroy
 
   ranks :row_order, :with_same => :board_id
 end
