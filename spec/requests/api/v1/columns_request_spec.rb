cc4e658e993fd4df5903d7fb5d6454abc3184f51
kijima <norimaking777@gmail.com>
2021-01-17 22:35:31 +0900

    Add sort spec

--- spec/requests/api/v1/columns_request_spec.rb
+++ spec/requests/api/v1/columns_request_spec.rb
@@ -26,6 +26,15 @@ RSpec.describe "Api::V1::Columns", type: :request do
         expect(json.length).to eq(0)
       end
     end
+    it 'sort by row_order' do
+      board0 = create(:column, board: board, row_order: 100)
+      board1 = create(:column, board: board, row_order: 0)
+
+      get "/api/v1/columns/#{user.id}"
+      json = JSON.parse(response.body)
+      expect(json[0]["id"]).to eq(board1.id) # 0
+      expect(json[1]["id"]).to eq(board0.id) # 100
+    end
 
     it '#index' do
 
