d3471371a02a43b890053116ddef10c1c89c3823
kijima <norimaking777@gmail.com>
2021-01-17 18:07:47 +0900

    Add spec

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Board, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Board do
+    it "is valid with user" do
+      user = create(:user)
+      board = create(:board, user: user)
+      expect(board).to be_valid
+    end
+  end
 end
