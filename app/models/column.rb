6c625f8487001e1138cdb534ea25e9796f4e1b56
kijima <norimaking777@gmail.com>
2021-01-17 16:54:48 +0900

    Add space

--- app/models/column.rb
+++ app/models/column.rb
@@ -1,7 +1,7 @@
 class Column < ApplicationRecord
   include RankedModel
   belongs_to :board
-  has_many :tasks, ->{ order('row_order asc') }, dependent: :destroy
+  has_many :tasks, -> { order('row_order asc') }, dependent: :destroy
 
   ranks :row_order, :with_same => :board_id
 end
