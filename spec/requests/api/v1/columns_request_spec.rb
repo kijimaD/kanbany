74dc4439d29a19085f5ee9486ca7dd4c268a522a
kijima <norimaking777@gmail.com>
2021-01-17 21:49:57 +0900

    Add index spec

--- spec/requests/api/v1/columns_request_spec.rb
+++ spec/requests/api/v1/columns_request_spec.rb
@@ -2,4 +2,33 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Columns", type: :request do
 
+  describe '#index' do
+    let!(:user) { create(:user) }
+    let!(:board) { create(:board, user: user) }
+
+    context 'exist column' do
+      specify do
+        create(:column, board: board)
+
+        get "/api/v1/columns/#{user.id}"
+        json = JSON.parse(response.body)
+
+        expect(response.status).to eq(200)
+        expect(json.length).to eq(1)
+      end
+    end
+    context 'not exist column' do
+      specify do
+        get "/api/v1/columns/#{user.id}"
+        json = JSON.parse(response.body)
+
+        expect(response.status).to eq(200)
+        expect(json.length).to eq(0)
+      end
+    end
+
+    it '#index' do
+
+    end
+  end
 end
