fcb6713af3112fecbefc45469c7b7b96ab24eaa6
kijima <norimaking777@gmail.com>
2021-02-01 22:27:31 +0900

    Fix test

--- spec/systems/index_spec.rb
+++ spec/systems/index_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'top_page', type: :system, js: true do
   it 'Display top page' do
+    create(:user)
+    user = create(:user)
+    create(:board, user: user)
     visit '/'
     expect(page).to have_content('Kanbany')
   end
