6f1b84bb78ef3b025e9f18614a84ad8ef453b2c4
kijima <norimaking777@gmail.com>
2021-02-01 09:35:56 +0900

    Add card specs

--- spec/systems/kanban_spec.rb
+++ spec/systems/kanban_spec.rb
@@ -4,17 +4,26 @@ RSpec.describe 'kanban', type: :system, js: true do
   let!(:user) { create(:user) }
   let!(:board) { create(:board, user: user) }
   let!(:column) { create(:column, board: board) }
-  let!(:task) { create(:task, column: column) }
+  # let!(:task) { create(:task, column: column) }
 
   before do
     visit '/develop'
   end
 
-  scenario 'Can edit cards' do
+  scenario 'Card' do
+    # カードが存在しない
+    expect(page).not_to have_field('Description', with: '')
+    expect(page).not_to have_field('Category', with: '')
+
+    # カードを追加できる
+    click_button 'add'
+    expect(page).to have_field('Description', with: '')
+    expect(page).to have_field('Category', with: '')
+
     # カードを編集できる
     fill_in 'Description', with: 'This is Description'
     fill_in 'Category', with: 'This is Category'
-    sleep 2
+    sleep 1
 
     # 入力が保存できる
     visit '/develop'
@@ -22,18 +31,15 @@ RSpec.describe 'kanban', type: :system, js: true do
     expect(page).to have_field('Category', with: 'This is Category')
     expect(page).not_to have_field('Description', with: 'MyString')
     expect(page).not_to have_field('Category', with: 'MyString')
-    sleep 1
 
     # コピーできる
     click_button 'whatshot'
     click_button 'copy_all'
     expect(page).to have_field('Description', with: '')
     expect(page).to have_field('Category', with: 'This is Category')
-    sleep 1
 
     # 削除できる
     click_button 'delete_sweep'
     expect(page).not_to have_field('Description', with: 'This is Description')
-    sleep 1
   end
 end
